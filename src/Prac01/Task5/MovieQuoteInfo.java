34fee56b6739ada92f08c54f79d59c2c7bc60ba2
adamcowie <adam.cowie1@my.jcu.edu.au>
2016-08-02 21:14:46 +1000

    Adam Cowie 1/8/16

--- src/Prac01/Task5/MovieQuoteInfo.java
+++ src/Prac01/Task5/MovieQuoteInfo.java
@@ -1,14 +1,15 @@
 public class MovieQuoteInfo
 {
-    private int count=4;
-    private String[] myMovie = new String[count];
     public static void main(String[] args)
     {
+        int count=4;
+        String[] myMovie = new String[count];
+
         myMovie[0]="Ill Be Back!";
         myMovie[1]="The Terminator";
         myMovie[2]="T800";
         myMovie[3]="1984";
-        for(int i=1; i<count; i++)
+        for(int i=0; i<count; i++)
         {
             System.out.println(myMovie[i]);
         }
--- src/Prac01/Task5/TableAndChairs.java
+++ src/Prac01/Task5/TableAndChairs.java
@@ -1,16 +1,18 @@
 public class TableAndChairs
 {
-    private int count=4;
-    private String[] tableChairs = new String[count];
     public static void main(String[] args)
     {
+        int count=4;
+        String[] tableChairs = new String[count];
+
         tableChairs[0]="X                      X";
         tableChairs[1]="X      XXXXXXXXXX      X";
         tableChairs[2]="XXXXX  X        X  XXXXX";
         tableChairs[3]="X   X  X        X  X   X";
-        for(int i=1; i<6; i++)
+
+        for(int i=1; i<7; i++)
         {
-            if (i<2){System.out.println(tableChairs[0]);}
+            if (i<3){System.out.println(tableChairs[0]);}
             if (i==3){System.out.println(tableChairs[1]);}
             if (i==4){System.out.println(tableChairs[2]);}
             if (i>4){System.out.println(tableChairs[3]);}
